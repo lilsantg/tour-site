9c202a5ad4133901c91cb14a2b643de96c6c9a99
lilsantg <lebedevv23@mail.ru>
2021-11-02 02:54:56 +0300

    create: added swiper-slider in hotel-section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const mySwiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+});
